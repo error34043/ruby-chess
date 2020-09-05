fdbd06fbdcaeecee16fced6eb73b4245c41681e8
Sanjana <sanjhemdev@gmail.com>
2020-09-06 04:56:11 +0530

    Add arrays for black and white pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
 
+require_relative 'string.rb'
+
+class Board
+  attr_reader :WHITE_PIECES, :BLACK_PIECES
+
+  WHITE_PIECES = ['♔', '♕', '♖', '♗', '♘', '♙']
+  BLACK_PIECES = ['♚'.black, '♛'.black, '♜'.black, '♝'.black, '♞'.black, '♟︎'.black]
+end
